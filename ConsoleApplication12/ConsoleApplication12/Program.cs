06d92f7b421440bce90035fae5599d57dcc64c70
kabirov082 <kabirov08@ya.ru>
2016-11-24 13:25:17 +0400

    c2

--- ConsoleApplication12/ConsoleApplication12/Program.cs
+++ ConsoleApplication12/ConsoleApplication12/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication12
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("текст1");
+            Console.WriteLine("k1");
             
             Console.WriteLine("a");
         }
